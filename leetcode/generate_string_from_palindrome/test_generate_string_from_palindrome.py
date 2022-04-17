c31b626c8157d4fe708b809ef84c3c0b471db1c2
Williano <paawilly17@gmail.com>
2022-04-16 23:15:16 -0500

    Add test

--- leetcode/generate_string_from_palindrome/test_generate_string_from_palindrome.py
+++ leetcode/generate_string_from_palindrome/test_generate_string_from_palindrome.py
@@ -7,8 +7,10 @@ class TestGeneratePalindrome(unittest.TestCase):
         self.test = Solution()
         self.data_1 = "AAAACACBA"
         self.data_2 = "RARARARARA"
+        self.data_3 = "A"
 
     def test_generate_palindrome(self):
 
         self.assertEqual(self.test.generate_palindrome(self.data_1), "AAACBCAAA")
         self.assertEqual(self.test.generate_palindrome(self.data_2), None)
+        self.assertEqual(self.test.generate_palindrome(self.data_3), "A")
